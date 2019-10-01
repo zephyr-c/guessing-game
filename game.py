b6e5182756417ce3e3f7d87e4622b82d0ad9f01c
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-01 12:31:30 -0700

    Imported randint and finished greeting

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 """A number-guessing game."""
 
 # Put your code here
+from random import randint
 print("Hello!")
-name = input("What is your name?")
+name = input("What is your name? ")
+print(f"Hi {name}! I'm thinking of a number between 1 and 100. Guess what it is!")
