723e76984ba0cea37fe47c75f4943796e6da4d4f
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-01 15:51:34 -0700

    Check Number Function WIP

--- game.py
+++ game.py
@@ -1,5 +1,17 @@
 """A number-guessing game."""
+def checknumber(n):
+    while n.isdigit() == False:
+        print("Invalid input. Please enter a while number!")
+        n = input("")
+    un = int(n)
+    return un 
+#def checkrange(x):
+ #   while x < 1 or x > 100:
+  #      print("Invalid input, try again!")
+  #      x = input("")
+  #      x = checknumber(x)
 
+    
 # Put your code here
 from random import randint
 print("Hello!")
@@ -7,25 +19,21 @@ name = input("What is your name? ")
 print(f"Hi {name}! I'm thinking of a number between 1 and 100. Guess what it is!")
 
 usernumber = input("")
-while usernumber.isdigit() == False:
-    print("Invalid input. Please enter a whole number!")
-    usernumber = input("")
+secretnumber = 50#randint(1,100)
 
-usernumber = int(usernumber)
-secretnumber = 4 #randint(1,100)
+#print(secretnumber)
 
 count = 1
 while usernumber != secretnumber:
-    while usernumber < 1 or usernumber > 100:
-        print("Invalid input, try again!")
-        usernumber = int(input(""))
+    usernumber = checknumber(usernumber)
 
     if usernumber > secretnumber:
         print("Too High!")
-        usernumber = int(input("Guess Again! "))
+        usernumber = input("Guess Again! ")
+
     elif usernumber < secretnumber:
         print("Too Low!")
-        usernumber = int(input("Guess Again!"))
+        usernumber = input("Guess Again!")
     count += 1
 
 print(f"That's right! You got it in {count} tries!")
