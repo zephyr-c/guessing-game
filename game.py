369a9583c03f73569e89b0e8f6ccc8879dc6d616
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-01 12:40:42 -0700

    Loop over user guesses, print responses

--- game.py
+++ game.py
@@ -5,3 +5,17 @@ from random import randint
 print("Hello!")
 name = input("What is your name? ")
 print(f"Hi {name}! I'm thinking of a number between 1 and 100. Guess what it is!")
+
+usernumber = int(input(""))
+secretnumber = 4 #randint(1,100)
+
+while usernumber != secretnumber:
+    if usernumber > secretnumber:
+        print("Too High!")
+        usernumber = int(input("Guess Again! "))
+    elif usernumber < secretnumber:
+        print("Too Low!")
+        usernumber = int(input("Guess Again!"))
+
+print("That's right! You got it in x tries!")
+
